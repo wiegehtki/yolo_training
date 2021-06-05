aca9d0f6ef96e87371d91101620dac107fd48b55
wiegehtki.de <58086193+wiegehtki@users.noreply.github.com>
2021-06-05 23:05:34 +0200

    Update

--- darknet.repo/training_carrera.sh
+++
@@ -1,2 +0,0 @@
-./darknet detector train ~/darknet/data/obj.data ~/darknet/cfg/yolo-obj.cfg ~/darknet/data/darknet53.conv.74
-
--- darknet.repo/webcam.sh
+++ darknet.repo/webcam.sh
@@ -1,6 +1,1 @@
-# Objekterkennung mit YOLOv4
-./darknet detector demo ./cfg/coco.data ./cfg/yolov4.cfg ./YoloWeights/yolov4.weights -c 0
-
-# Beispiel für YOLOv3
-#./darknet detector demo ./cfg/coco.data ./cfg/yolov3-tiny.cfg ./YoloWeights/yolov3-tiny.weights http://192.168.254.62:8080/video?dummy=param.mjpg -i 0
-
+./darknet detector demo ~/darknet/data/obj.data ./cfg/autorennbahn.cfg ./backup/autorennbahn_final.weights -c 2
